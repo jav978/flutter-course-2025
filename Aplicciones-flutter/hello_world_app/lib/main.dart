19edc87311d9cb052d558234e813fa2e579f9d78
José Antonio Vásquez <jvasquez978@gmail.com>
2025-08-12 16:40:03 -0400

    Actualizando counter_screen

---
+++ Aplicciones-flutter/hello_world_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_function_screen.dart';
+
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,  
+      theme: ThemeData(useMaterial3: true,
+      colorSchemeSeed: Colors.cyanAccent),
+      home: CounterFunctionScreen()
+    );
+  }
+}
---
+++ Aplicciones-flutter/hello_world_app/lib/presentation/screens/counter/counter_function_screen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class CounterFunctionScreen extends StatefulWidget {
+  const CounterFunctionScreen({super.key});
+
+  @override
+  State<CounterFunctionScreen> createState() => _CounterFunctionScreenState();
+}
+
+class _CounterFunctionScreenState extends State<CounterFunctionScreen> {
+  int clickCounter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const FlutterLogo(),
+            const SizedBox(width: 10),
+            const Text('Counter Function', style: TextStyle(fontSize: 25)),
+          ],
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh_rounded),
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickCounter',
+              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+            Text(
+              'Click${clickCounter > 1 ? 's' : ''}',
+              style: TextStyle(fontSize: 25),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CustomButton(
+            icon: Icons.refresh_rounded,
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
+            },
+          ),
+          const SizedBox(height: 10),
+          CustomButton(
+            icon: Icons.exposure_minus_1_outlined,
+            onPressed: () {
+              setState(() {
+                if (clickCounter == 0) return;
+                clickCounter--;
+              });
+            },
+          ),
+          const SizedBox(height: 10),
+          CustomButton(
+            icon: Icons.plus_one,
+            onPressed: () {
+              setState(() {
+                clickCounter++;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback? onPressed;
+
+  const CustomButton({super.key, required this.icon, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      shape: const StadiumBorder(),
+      enableFeedback: true,
+      elevation: 5,
+      backgroundColor: Colors.blue,
+      onPressed: onPressed,
+      child: Icon(icon),
+    );
+  }
+}
